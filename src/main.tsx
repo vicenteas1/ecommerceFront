caee548d00aea310e1f117e32274309e10553b9b
vicenteas1 <vicenteas.estudios8059@gmail.com>
2025-10-27 04:55:03 -0300

    se agrega hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./main.scss";
 import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,7 +17,9 @@ createRoot(document.getElementById("root")!).render(
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <AppRoutes />
+          <HashRouter>
+            <AppRoutes />
+          </HashRouter>
         </ThemeProvider>
       </Provider>
     </ErrorBoundary>
