4816a6b171ad7b765c63d7b5651ab81b573eda6b
vicenteas1 <vicenteas.estudios8059@gmail.com>
2025-10-27 05:19:13 -0300

    Se agregan redirects a spa

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import "./main.scss";
 import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -17,9 +16,7 @@ createRoot(document.getElementById("root")!).render(
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <HashRouter>
-            <AppRoutes />
-          </HashRouter>
+          <AppRoutes />
         </ThemeProvider>
       </Provider>
     </ErrorBoundary>
